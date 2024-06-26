21968697466161e28610ee123c633ffcd4e15f6d
Lena Ramisch <lena.ramisch@gmail.com>
2024-06-26 09:40:20 +0200

    Build in chi render usage

--- src/go.mod
+++ src/go.mod
@@ -4,5 +4,8 @@ go 1.22.1
 
 require (
 	github.com/go-chi/chi/v5 v5.0.14
+	github.com/go-chi/render v1.0.3
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/ajg/form v1.5.1 // indirect
--- src/pkg/handler/handler.go
+++ src/pkg/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"example.SMSService.com/pkg/domain"
+	"github.com/go-chi/render"
 )
 
 func SMSPostRequest(res http.ResponseWriter, req *http.Request) {
@@ -12,19 +13,23 @@ func SMSPostRequest(res http.ResponseWriter, req *http.Request) {
 
 	err := json.NewDecoder(req.Body).Decode(&smsRequest)
 	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		render.Status(req, http.StatusBadRequest)
+		render.PlainText(res, req, "You messed up: "+err.Error())
 		return
 	}
 
 	if smsRequest.ToNumber == "" || smsRequest.MessageBody == "" {
 		http.Error(res, "Empty string content in either ToNumber or MessageBody", http.StatusBadRequest)
+		return
 	}
 	err = domain.SendSMS(smsRequest.ToNumber, smsRequest.MessageBody)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	res.WriteHeader(http.StatusOK)
-	res.Write([]byte("SMS sent successfully"))
+	render.Status(req, http.StatusOK)
+	render.PlainText(res, req, "SMS sent successfully")
+	return
 }
 
 func EmailPostRequest(res http.ResponseWriter, req *http.Request) {
@@ -38,11 +43,14 @@ func EmailPostRequest(res http.ResponseWriter, req *http.Request) {
 
 	if emailRequest.ToEmail == "" || emailRequest.ToName == "" || emailRequest.Subject == "" || emailRequest.MessageBody == "" {
 		http.Error(res, "Empty string content in either ToEmail, ToName, Subject or MessageBody", http.StatusBadRequest)
+		return
 	}
 	err = domain.SendEmail(emailRequest.ToEmail, emailRequest.ToName, emailRequest.Subject, emailRequest.MessageBody)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	res.WriteHeader(http.StatusOK)
-	res.Write([]byte("Email sent successfully"))
+	render.Status(req, http.StatusOK)
+	render.PlainText(res, req, "Email sent successfully")
+	return
 }
