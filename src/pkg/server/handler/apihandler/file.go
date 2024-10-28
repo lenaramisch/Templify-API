d1af8b1b38f250aa1d11e733cc6aa7c914fc28b4
Lena Ramisch <lena.ramisch@gmail.com>
2024-10-28 19:48:18 +0100

    Check for authorisation in every handler func

--- src/pkg/server/handler/apihandler/file.go
+++ src/pkg/server/handler/apihandler/file.go
@@ -11,6 +11,11 @@ import (
 // Download file from S3 bucket
 // (GET /file/download/{fileName})
 func (ah *APIHandler) GetDownloadFileURL(w http.ResponseWriter, r *http.Request, fileName string) {
+	requiredClaims := map[string]any{"role": "user"}
+	checkedAuthorization := ah.Authorizer.CheckIfAuthorised(w, r, requiredClaims)
+	if !checkedAuthorization {
+		return
+	}
 	downloadURL, err := ah.Usecase.GetFileDownloadURL(fileName)
 	if err != nil {
 		handler.HandleInternalServerError(w, r, err, "Error getting download URL")
@@ -24,6 +29,11 @@ func (ah *APIHandler) GetDownloadFileURL(w http.ResponseWriter, r *http.Request,
 // Upload file to S3 bucket for later use
 // (POST /file/upload)
 func (ah *APIHandler) GetUploadFileURL(w http.ResponseWriter, r *http.Request, fileName string) {
+	requiredClaims := map[string]any{"role": "user"}
+	checkedAuthorization := ah.Authorizer.CheckIfAuthorised(w, r, requiredClaims)
+	if !checkedAuthorization {
+		return
+	}
 	uploadURL, err := ah.Usecase.GetFileUploadURL(fileName)
 	if err != nil {
 		handler.HandleInternalServerError(w, r, err, "Error getting upload URL")
